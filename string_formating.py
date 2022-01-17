a63849ada1cddebfe89f5c55fe8e96ce59267a80
DHINESH KUMAR .S <94525449+Dineshpytho@users.noreply.github.com>
2022-01-17 11:49:51 +0530

    second_commit

--- string_formating.py
+++ string_formating.py
@@ -1,6 +1,3 @@
-from traceback import print_exception
-
-
 print("i'm going to inject %s here."%'something')
 print('\n')
 print ("i'm going to inject %s text here,and %s text here."%('some','more'))
