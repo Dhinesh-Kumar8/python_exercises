509aa12475f996dc6ccdcb880f4de7023a3c35d7
Dhinesh kumar <dhineshsurya0829@gmail.com>
2022-01-18 22:44:14 +0530

    firsttry

--- lists.py
+++ lists.py
@@ -1,2 +1,23 @@
-list1 =[1,2,3]
-list1.append('append me!')
+my_list =[1,2,3]
+my_list.append('append me!')
+my_list.pop(0)
+popped_item=my_list.pop()
+print(popped_item)   
+print(my_list)
+print(my_list[0])
+print(len(my_list))
+print("\n")
+my_lists =["one","two","three",4,5]
+print(my_lists[1:])
+print(my_lists[:3])
+print("\n")
+print(my_lists + ['new item'])
+print("\n")
+my_lists = my_lists+["add new item permant"]
+print(my_lists)
+print(my_lists*2)
+print("\n")
+new_list2 = ['a','b','c','d']
+new_list2 = new_list2.reverse()
+print("new_list2 ")
+
