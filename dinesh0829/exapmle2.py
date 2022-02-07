48e92fefe5b40d1318b66e789fc9699b2f8e565a
Dhinesh kumar <dhineshsurya0829@gmail.com>
2022-02-07 13:34:43 +0530

    hi

--- dinesh0829/exapmle2.py
+++ dinesh0829/exapmle2.py
@@ -1,6 +1,11 @@
-my_text = ["1","2","3","4","5"]
-i = 0
-while i< len(my_text):
- my_text [i].replace ("enter ","the ","word")
-i = i + 1
-print(my_text)
+Strings=["1","2","3","4","5"]
+new_strings=[]
+for string in Strings:
+    new_string =string.replace("1","var")#modify old string
+    new_strings.append(new_string)
+    print(new_strings)
+    print("\n")
+my_str = [1,2,3]
+print(my_str)
+my_str.reverse()
+print(my_str)
