3af2e145d62501c2646e63247630c0d2a12a6753
Dhinesh kumar <dhineshsurya0829@gmail.com>
2022-05-04 06:04:01 +0530

    9

--- homework.py
+++ homework.py
@@ -1,21 +1,8 @@
-from operator import index
-
-
 list = [10,20,30,10,30,100,10]
 num = sorted(list)
 n=3
 splited = [list[i::n] for i in range(n)]
 print(splited)
 print((num))
-for i in splited:
-        if i == int ("i", splited):
-            print((i,i,i))
-        else :
-            i == int
-        print ((i))
+
     
-list = [10,20,30,10,30,100,10]
-for num in splited:
-    for i in splited:
-        i==i,i,i
-    print([i])
