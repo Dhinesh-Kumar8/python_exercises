b75f4e39ed10e3ac0d3e858e8ed5035b84a49f9c
Dhinesh kumar <dhineshsurya0829@gmail.com>
2022-05-08 07:53:24 +0530

    0

--- flash/file.py
+++ flash/file.py
@@ -1,8 +1,14 @@
 from tokenize import Number
 list = [10,20,30,10,30,100,10]
 Number = sorted(list)
+mylist = []
 print((Number))
 for num in Number :
-    for index in num:
-        print(num)
-    print([])
+    first_Number = num
+    for mylist in Number:
+        second_number = Number
+        if num == Number:
+            print(second_number)
+        else:
+            print(first_Number)
+        
